928ac4a0654a4239c9a6a14c1f17b2d15de3ec84
Dkid555 <dimason2008@gmail.com>
2023-06-08 11:57:41 -0400

    06.08.2023

--- java-test/src/test/java/com/leet_easy/RemoveDupFromLLTest.java
+++ java-test/src/test/java/com/leet_easy/RemoveDupFromLLTest.java
@@ -1,13 +1,12 @@
 package com.leet_easy;
 
-//import com.leet_easy.RemoveDupFromLL;
 
 import junit.framework.TestCase;
 
 public class RemoveDupFromLLTest extends TestCase {
     RemoveDupFromLL remove = new RemoveDupFromLL();
 
-    public void testRemoveDuplic() {
+    public void testRemoveDuplic1() {
 
         ListNode res = remove.removeDuplic(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3)))));
         for (int x : new int[]{1, 2, 3}
@@ -36,4 +35,24 @@ public class RemoveDupFromLLTest extends TestCase {
         ListNode res3 = remove.removeDuplic(new ListNode(10, new ListNode(10, new ListNode(11, new ListNode(13, new ListNode(14, new ListNode(51)))))));
         assertEquals("10,11,13,14,51", res3.toString().replaceAll(" ", ""));
     }
+
+    public void testRemoveDuplic4() {
+        ListNode res4 = remove.removeDuplic(new ListNode());
+        assertEquals("0", res4.toString().replaceAll(" ", ""));
+    }
+
+    public void testRemoveDuplic5() {
+        ListNode res5 = remove.removeDuplic(new ListNode(10));
+        assertEquals("10", res5.toString().replaceAll(" ", ""));
+    }
+
+    public void testRemoveDuplic6() {
+        ListNode res6 = remove.removeDuplic(new ListNode(1, new ListNode(1, new ListNode(2,
+                new ListNode(10, new ListNode(10, new ListNode(15,
+                        new ListNode(51, new ListNode(51,
+                                new ListNode(51, new ListNode(100,
+                                        new ListNode(100, new ListNode(101)))))))))))));
+        assertEquals("1,2,10,15,51,100,101", res6.toString().replaceAll(" ", ""));
+    }
+
 }
