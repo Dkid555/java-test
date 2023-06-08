9a1b3ee8be543d0323246848ca4f839f60c91431
Dkid555 <dimason2008@gmail.com>
2023-06-08 12:17:21 -0400

    06.08.2023

--- java-test/src/test/java/com/leet_easy/FibonacciNumberTest.java
+++ java-test/src/test/java/com/leet_easy/FibonacciNumberTest.java
@@ -6,7 +6,7 @@ public class FibonacciNumberTest extends TestCase {
 
     public void testFibonachi1() {
         int[] res = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181};
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < res.length; i++) {
             assertEquals(res[i], FibonacciNumber.Fibonachi(i));
         }
     }
