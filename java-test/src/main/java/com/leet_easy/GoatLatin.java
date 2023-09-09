34d0bdc20cae3c713dce36c632f1369b5641cf4c
Dkid555 <dimason2008@gmail.com>
2023-09-09 12:27:37 -0400

    09.08.2023

---
+++ java-test/src/main/java/com/leet_easy/GoatLatin.java
@@ -0,0 +1,27 @@
+package com.leet_easy;
+
+import java.util.Set;
+
+public class GoatLatin {
+    public static String toGoatLatin(String sentence) {
+        Set<Character> vowel = Set.of('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'U', 'u');
+        String[] letter = sentence.split(" ");
+        String res = "";
+        String a = "a";
+        for (String word : letter) {
+            if (vowel.contains(word.charAt(0))) {
+                word = word + "ma" + a;
+            } else {
+                String begin = word.substring(0, 1);
+                word = word.substring(1) + begin + "ma" + a;
+            }
+            a += "a";
+            res += word + " ";
+        }
+        return res.trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toGoatLatin("I speak Goat Latin"));
+    }
+}
