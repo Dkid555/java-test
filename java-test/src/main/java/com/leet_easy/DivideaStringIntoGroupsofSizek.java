e4cf9fa70e7a307247ac49f7b2865dbd5bf3d5b1
Dkid555 <dimason2008@gmail.com>
2023-09-14 09:07:53 -0400

    09.14.2023

---
+++ java-test/src/main/java/com/leet_easy/DivideaStringIntoGroupsofSizek.java
@@ -0,0 +1,38 @@
+package com.leet_easy;
+
+import java.util.Arrays;
+
+public class DivideaStringIntoGroupsofSizek {
+
+    public static String[] divideString(String s, int k, char fill) {
+        int length = s.length() % k == 0 ? s.length() / k : s.length() / k + 1;
+        String[] res = new String[length];
+        int i = 0;
+        int index = 0;
+        for (; i < s.length(); i += k) {
+            if (i + k > s.length())
+                break;
+            res[index++] = s.substring(i, i + k);
+        }
+        if (i < s.length()) {
+            res[index] = s.substring(i);
+            res[index] = res[index] + String.valueOf(fill).repeat(k - res[index].length());
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(divideString("abcdefghij", 3, 'x')));
+    }
+}
+//A string s can be partitioned into groups of size k using the following procedure:
+//
+//The first group consists of the first k characters
+// of the string, the second group consists of the next k characters of the string,
+// and so on. Each character can be a part of exactly one group.
+//For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
+//Note that the partition is done so that after removing the fill character from the last group (if it exists)
+// and concatenating all the groups in order, the resultant string should be s.
+//
+//Given the string s, the size of each group k and the character fill, return a string array denoting
+// the composition of every group s has been divided into, using the above procedure.
--- java-test/src/main/java/com/leet_easy/PositionsofLargeGroups.java
+++ java-test/src/main/java/com/leet_easy/PositionsofLargeGroups.java
@@ -1,17 +1,36 @@
 package com.leet_easy;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PositionsofLargeGroups {
-    public List<List<Integer>> largeGroupPositions(String s) {
+    public static List<List<Integer>> largeGroupPositions(String s) {
         List<List<Integer>> res = new ArrayList<>();
-        if(s.length() < 3) return new ArrayList<>();
+        Deque<Integer> temp = new ArrayDeque<>();
+        int count = 1;
+        if (s.length() < 3) return new ArrayList<>();
         int prev = s.charAt(0);
-        for (int i = 1; i < s.length(); i++){
-            if(prev == s.charAt(i)){
-                
+        for (int i = 1; i < s.length(); i++) {
+            if (prev == s.charAt(i)) {
+                ++count;
+                if (count == 2) {
+                    temp.addFirst(i - 1);
+                }
+                temp.addLast(i);
+            } else {
+                prev = s.charAt(i);
+                if (count > 2)
+                    res.add(Arrays.asList(temp.getFirst(), temp.getLast()));
+                count = 1;
+                temp = new ArrayDeque<>();
             }
         }
+        if (count > 2 && !temp.isEmpty()) {
+            res.add(Arrays.asList(temp.getFirst(), temp.getLast()));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largeGroupPositions("aaa"));
     }
 }
