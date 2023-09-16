fe751dd9f3abd215c1e901e9fe4b8cbdbed5a223
Dkid555 <dimason2008@gmail.com>
2023-09-16 14:28:28 -0400

    09.16.2023

---
+++ java-test/src/main/java/com/leet_easy/FindtheLoserstheCircularGame.java
@@ -0,0 +1,26 @@
+package com.leet_easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindtheLoserstheCircularGame {
+    public static int[] circularGameLosers(int n, int k) {
+        Set<Integer> friends = new HashSet<>();
+        int i = 0, rem = 0;
+        while (true) {
+            rem = (rem + i++ * k) % n;
+            if (!friends.add(rem)) break;
+        }
+        int[] result = new int[n - friends.size()];
+        int j = 0;
+        for (i = 0; i < n; i++) {
+            if (!friends.contains(i)) result[j++] = i + 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(circularGameLosers(4, 4)));
+    }
+}
---
+++ java-test/src/main/java/com/leet_easy/SurfaceAreaof3DShapes.java
@@ -0,0 +1,21 @@
+package com.leet_easy;
+
+public class SurfaceAreaof3DShapes {
+    public static int surfaceArea(int[][] grid) {
+        int res = 0;
+        for (int row = 0; row < grid.length; row++)
+            for (int column = 0; column < grid[0].length; column++) {
+                if (grid[row][column] > 0)
+                    res += 4 * grid[row][column] + 2;
+                if (row > 0)
+                    res -= Math.min(grid[row][column], grid[row - 1][column]) * 2;
+                if (column > 0)
+                    res -= Math.min(grid[row][column], grid[row][column - 1]) * 2;
+            }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(surfaceArea(new int[][]{{1, 2}, {3, 4}}));
+    }
+}
