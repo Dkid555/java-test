352e42585ca36d68a3362535174bf7b56fcb7954
Dkid555 <dimason2008@gmail.com>
2023-10-05 11:58:35 -0400

    10.05.2023

---
+++ java-test/src/main/java/com/leet_easy/CountHillsandValleysinanArray.java
@@ -0,0 +1,18 @@
+package com.leet_easy;
+
+public class CountHillsandValleysinanArray {
+    public static int countHillValley(int[] nums) {
+        int r = 0, left = nums[0];
+        for (int i = 1; i < nums.length - 1; i++)
+            if (left < nums[i] && nums[i] > nums[i + 1] || left > nums[i] && nums[i] < nums[i + 1]) {
+                r++;
+                left = nums[i];
+            }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{6, 6, 5, 5, 4, 1};
+        System.out.println(countHillValley(nums));
+    }
+}
