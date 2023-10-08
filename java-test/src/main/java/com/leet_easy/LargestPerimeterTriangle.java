17bd016fdfcf1a5e2cc9a1add7838118a550dda0
Dkid555 <dimason2008@gmail.com>
2023-10-08 04:02:29 -0400

    10.08.2023

---
+++ java-test/src/main/java/com/leet_easy/LargestPerimeterTriangle.java
@@ -0,0 +1,29 @@
+package com.leet_easy;
+
+import java.util.Arrays;
+
+public class LargestPerimeterTriangle {
+    public int largestPerimeter(int[] nums) {
+        if (nums.length < 3)
+            return 0;
+//        int max = 0;
+//        for (int side1 = 0; side1 < nums.length; side1++)
+//            for (int side2 = side1 + 1; side2 < nums.length; side2++)
+//                for (int side3 = side2 + 1; side3 < nums.length; side3++){
+//                    if(nums[side2] < nums[side1] + nums[side3] && nums[side1] < nums[side2] + nums[side3] && nums[side3] < nums[side2] + nums[side1])
+//                        max = Math.max(nums[side1] + nums[side2] + nums[side3], max);
+//                }
+//        return max;
+
+
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            int side1 = i;
+            int side2 = i - 1;
+            int side3 = i - 2;
+            if (nums[side2] < nums[side1] + nums[side3] && nums[side1] < nums[side2] + nums[side3] && nums[side3] < nums[side2] + nums[side1])
+                return nums[side1] + nums[side2] + nums[side3];
+        }
+        return 0;
+    }
+}
---
+++ java-test/src/main/java/com/leet_easy/VerifyingAlienDictionary.java
@@ -0,0 +1,37 @@
+package com.leet_easy;
+
+public class VerifyingAlienDictionary {
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] charMap = new int[26];
+        for (int i = 0; i < order.length(); i++) {
+            //setting the index what it in there alphabet (in order)
+            charMap[order.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            if (compare(words[i], words[i + 1], charMap) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int compare(String word1, String word2, int[] charMap) {
+        int i = 0;
+        int j = 0;
+        int difference = 0;
+
+        while (i < word1.length() && j < word2.length() && difference == 0) {
+            difference = charMap[word1.charAt(i) - 'a'] - charMap[word2.charAt(i) - 'a'];
+            i++;
+            j++;
+        }
+
+        if (difference == 0) {
+            //when word1 length will be smaller than word2 it will return true as we have gone through whole while loop (difference value is zero)
+            return word1.length() - word2.length();
+        } else {
+            return difference;
+        }
+    }
+}
