ef37a9e6f8276d6853d25e63adf17b802b4221a7
Dkid555 <dimason2008@gmail.com>
2023-10-18 09:09:27 -0400

    10.17.2023

---
+++ java-test/src/main/java/com/leet_easy/DestinationCity.java
@@ -0,0 +1,21 @@
+package com.leet_easy;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DestinationCity {
+    public String destCity(List<List<String>> paths) {
+        Set<String> cities = new HashSet<>();
+        for (List<String> path : paths) {
+            cities.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            String dest = path.get(1);
+            if (!cities.contains(dest)) {
+                return dest;
+            }
+        }
+        return "";
+    }
+}
