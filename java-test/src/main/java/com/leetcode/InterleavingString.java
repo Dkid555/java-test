8ac3e315bcccf71b6fdab48d33d3fe34a1fccaa0
Dkid555 <dimason2008@gmail.com>
2023-10-23 23:50:32 -0400

    10.23.2023

---
+++ java-test/src/main/java/com/leetcode/InterleavingString.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+public class InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length(), l = s3.length();
+        if (m + n != l) return false;
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int j = 1; j <= n; ++j) {
+            dp[j] = (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1));
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            dp[0] = (dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1));
+            for (int j = 1; j <= n; ++j) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        return dp[n];
+    }
+}
+//Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+//
+//An interleaving of two strings s and t is a configuration where s and t are divided into n and m
+//substrings
+// respectively, such that:
+//
+//s = s1 + s2 + ... + sn
+//t = t1 + t2 + ... + tm
+//|n - m| <= 1
+//The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+//Note: a + b is the concatenation of strings a and b.
