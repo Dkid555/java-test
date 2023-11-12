0752eecde7d34f12b9a5b059b6e34d5107b15f87
Dkid555 <dimason2008@gmail.com>
2023-11-12 03:52:42 -0500

    11.7.2023

---
+++ java-test/src/main/java/com/leetcode/MaximumGap.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class MaximumGap {
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+
+        if (n < 2)
+            return 0;
+
+        int min = Integer.MAX_VALUE;
+        int max = -1;
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        int interval = (int) (Math.ceil((max - min + 0.0) / (n - 1)));
+        if (interval == 0)
+            return 0;
+        int[] bucketMin = new int[n];
+        int[] bucketMax = new int[n];
+        Arrays.fill(bucketMin, Integer.MAX_VALUE);
+        Arrays.fill(bucketMax, Integer.MIN_VALUE);
+        for (int num : nums) {
+            int index = (num - min) / interval;
+            bucketMin[index] = Math.min(bucketMin[index], num);
+            bucketMax[index] = Math.max(bucketMax[index], num);
+        }
+        int prev = min;
+        int maxGap = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (bucketMin[i] == Integer.MAX_VALUE)
+                continue;
+
+            int gap = bucketMin[i] - prev;
+            maxGap = Math.max(maxGap, gap);
+
+            prev = bucketMax[i];
+        }
+
+        return maxGap;
+    }
+}
+//Given an integer array nums, return the maximum difference between two successive
+// elements in its sorted form. If the array contains less than two elements, return 0.
+//
+//You must write an algorithm that runs in linear time and uses linear extra space.
---
+++ java-test/src/main/java/com/leetcode/UniqueEmailAddresses.java
@@ -0,0 +1,62 @@
+package com.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueEmailAddresses {
+    public static int numUniqueEmails(String[] emails) {
+        Set<String> unique = new HashSet<>();
+        for (String email : emails) {
+            String[] emailSplit = email.split("@");
+            String name = emailSplit[0];
+            name = name.replaceAll("\\.", "");
+            if (name.contains("+")) {
+                name = name.substring(0, name.indexOf("+"));
+//                System.out.println(name);
+            }
+//            System.out.println(name + "@"+ emailSplit[1]);
+            unique.add(name + "@" + emailSplit[1]);
+        }
+        return unique.size();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(numUniqueEmails(new String[]{"test.email+alex@leetcode.com", "test.email.leet+alex@code.com"}));
+    }
+
+    public int numUniqueEmails2(String[] emails) {
+        Set<String> unique = new HashSet<>();
+        for (String email : emails) {
+            unique.add(formatEmail(email));
+        }
+        return unique.size();
+    }
+
+    private String formatEmail(String s) {
+        int i = 0;
+        int j = 0;
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        boolean check = false;
+        while (i < n - 4) {
+            if (!check) {
+                if (chars[i] == '.') {
+                    i++;
+                }
+                if (chars[i] == '+') {
+                    while (chars[i] != '@') {
+                        i++;
+                    }
+                }
+            }
+            if (chars[i] == '@') {
+                check = true;
+            }
+            chars[j] = chars[i];
+            i++;
+            j++;
+        }
+        return String.valueOf(chars, 0, j);
+    }
+}
