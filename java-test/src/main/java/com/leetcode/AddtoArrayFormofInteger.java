49abf0fa2a84190ae57691d508c053eaff6f8910
Dkid555 <dimason2008@gmail.com>
2023-11-16 00:46:50 -0500

    15.7.2023

---
+++ java-test/src/main/java/com/leetcode/AddtoArrayFormofInteger.java
@@ -0,0 +1,41 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AddtoArrayFormofInteger {
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> numK = Arrays.stream(num).boxed().collect(Collectors.toList());
+        List<Integer> kString = Arrays.stream(String.valueOf(k).split("")).map(Integer::parseInt).collect(Collectors.toList());
+        return numK.size() >= kString.size() ? helper(numK, kString) : helper(kString, numK);
+    }
+
+    public static List<Integer> helper(List<Integer> arr1, List<Integer> arr2) {
+        // arr1 >= arr2;
+        int size1 = arr1.size() - 1;
+        int size2 = arr2.size() - 1;
+        int fromPrev = 0;
+        while (size1 >= 0 && size2 >= 0) {
+            int temp = arr1.get(size1) + arr2.get(size2--) + fromPrev;
+            fromPrev = temp / 10 > 0 ? 1 : 0;
+            temp = temp % 10;
+            arr1.set(size1--, temp);
+        }
+        if (fromPrev != 0) {
+            while (size1 >= 0) {
+                int temp = arr1.get(size1) + fromPrev;
+                fromPrev = temp / 10 > 0 ? 1 : 0;
+                temp = temp % 10;
+                arr1.set(size1--, temp);
+            }
+            if (fromPrev > 0)
+                arr1.add(0, fromPrev);
+        }
+        return arr1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addToArrayForm(new int[]{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}, 1));
+    }
+}
---
+++ java-test/src/main/java/com/leetcode/DeterminetheMinimumSumofakavoidingArray.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DeterminetheMinimumSumofakavoidingArray {
+    public int minimumSum(int n, int k) {
+        return k / 2 >= n ? n * (n + 1) / 2 : (n + (k - 1) / 2) * (n + (k - 1) / 2 + 1) / 2 - (k - 1) * (k - 1 + 1) / 2 + (k / 2) * (k / 2 + 1) / 2;
+    }
+
+    public int minimumSum2(int n, int k) {
+        Set<Integer> set = new HashSet<>();
+        int temp = 1, sum = 0;
+        while (set.size() != n) {
+            if (!set.contains(k - temp)) {
+                set.add(temp);
+                sum += temp;
+            }
+            temp++;
+        }
+        return sum;
+    }
+}
