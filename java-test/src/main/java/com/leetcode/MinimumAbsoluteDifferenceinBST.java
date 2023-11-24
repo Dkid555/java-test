9f8fdbc0c9bfd1e551876de746d09e8c6ee71c8f
Dkid555 <dimason2008@gmail.com>
2023-11-23 20:59:52 -0500

    11.23.2023

---
+++ java-test/src/main/java/com/leetcode/MinimumAbsoluteDifferenceinBST.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+public class MinimumAbsoluteDifferenceinBST {
+    int min = Integer.MAX_VALUE;
+    TreeNode previous;
+
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null)
+            return this.min;
+        getMinimumDifference(root.left);
+        if (previous != null)
+            this.min = Math.min(this.min, root.val - this.previous.val);
+        this.previous = root;
+        getMinimumDifference(root.right);
+        return this.min;
+    }
+}
---
+++ java-test/src/main/java/com/leetcode/MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,48 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MinimumDistanceBetweenBSTNodes {
+    public int minDiffInBST(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        diveInto(root, result);
+        Collections.sort(result);
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < result.size() - 1; ++i)
+            minDistance = Math.min(minDistance, result.get(i + 1) - result.get(i));
+        return minDistance;
+    }
+
+    private void diveInto(TreeNode root, List<Integer> result) {
+        if (root == null)
+            return;
+        result.add(root.val);
+        diveInto(root.left, result);
+        diveInto(root.right, result);
+    }
+
+
+    int min = Integer.MAX_VALUE;
+    TreeNode prevNode = null;
+
+    public int minDiffInBST2(TreeNode root) {
+        helper(root);
+        return min;
+    }
+
+    private void helper(TreeNode root) {
+        if (root == null) return;
+
+        helper(root.left);
+
+        if (prevNode != null) {
+            min = Math.min(min, root.val - prevNode.val);
+        }
+
+        prevNode = root;
+
+        helper(root.right);
+    }
+}
