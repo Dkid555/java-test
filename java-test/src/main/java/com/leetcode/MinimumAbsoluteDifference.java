dd8e6830b28503bcded4e5b70782d112906b31e0
Dkid555 <dimason2008@gmail.com>
2024-01-24 18:52:39 -0500

    from mac

--- java-test/src/main/java/com/leetcode/MinimumAbsoluteDifference.java
+++ java-test/src/main/java/com/leetcode/MinimumAbsoluteDifference.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MinimumAbsoluteDifference {
-    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+    static public List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
         int min = Integer.MAX_VALUE;
@@ -18,13 +18,14 @@ public class MinimumAbsoluteDifference {
 
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i + 1] - arr[i] == min) {
-                List<Integer> pair = new ArrayList<>(2);
-                pair.add(arr[i]);
-                pair.add(arr[i + 1]);
+                List<Integer> pair = List.of(arr[i], arr[i + 1]);
                 ans.add(pair);
-
             }
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minimumAbsDifference(new int[]{1,3,4,2}));
     }
 }
