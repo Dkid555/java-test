72722b168f213e67c216b1cd73a1b9f6a317de41
dkid <dimason2008usa@gmail.com>
2024-01-25 01:58:43 -0500

    from mac

---
+++ java-test/src/main/java/com/leetcode/DistanceBetweenBusStops.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+public class DistanceBetweenBusStops {
+    static int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        if(start > destination){
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+        int sum = 0;
+        for(int i = start; i <  destination; i++){
+            sum += distance[i];
+        }
+        int disSum = 0;
+        for(int dis : distance){
+            disSum += dis;
+        }
+        return Math.min(sum, disSum-sum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(distanceBetweenBusStops(new int[]{1,2,3,4}, 0 ,3));
+    }
+}
---
+++ java-test/src/main/java/com/leetcode/MinimumCosttoMoveChipstoTheSamePosition.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+public class MinimumCosttoMoveChipstoTheSamePosition {
+    public int minCostToMoveChips(int[] position) {
+        int length = position.length;
+        int odd = 0;
+        int even = 0;
+        for (int i = 0; i < length; i++) {
+            if (position[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        return Math.min(odd, even);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
