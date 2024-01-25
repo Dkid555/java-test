fc3cbfc4abc9cac5af7cd58b40d5ebdb1e5ffebf
dkid <dimason2008usa@gmail.com>
2024-01-24 19:00:34 -0500

    from mac

--- java-test/src/main/java/com/leetcode/MinimumAbsoluteDifference.java
+++ java-test/src/main/java/com/leetcode/MinimumAbsoluteDifference.java
@@ -1,28 +1,22 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MinimumAbsoluteDifference {
     static public List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
         int min = Integer.MAX_VALUE;
-
+        Map<Integer, List<List<Integer>>> map = new HashMap<>();
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i + 1] - arr[i] < min) {
                 min = arr[i + 1] - arr[i];
             }
+            List<List<Integer>> temp = map.getOrDefault((arr[i + 1] - arr[i]), new ArrayList<>());
+            temp.add(List.of(arr[i], arr[i + 1]));
+            map.put(arr[i + 1] - arr[i], temp);
         }
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i + 1] - arr[i] == min) {
-                List<Integer> pair = List.of(arr[i], arr[i + 1]);
-                ans.add(pair);
-            }
-        }
-        return ans;
+        return map.get(min);
     }
 
     public static void main(String[] args) {
