a3b4a7aa273d1ed8af82845f5d92034e126ddbdf
dkid <dimason2008usa@gmail.com>
2024-01-25 23:07:34 -0500

    from mac 01.25.2024

---
+++ java-test/src/main/java/com/leetcode/MinimumOperationsToMakeArrayEqual.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+public class MinimumOperationsToMakeArrayEqual {
+    public int minOperations(int n) {
+        return n % 2 == 0 ? (n / 2) * (n / 2) : ((n - 1) / 2) * ((n - 1) / 2);
+    }
+    public int minOperBetter(int n){
+        return n*n/4;
+    }
+}
+
+//You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid values of i (i.e., 0 <= i < n).
+//
+//In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and
+// add 1 to arr[y] (i.e., perform arr[x] -=1 and arr[y] += 1). The goal is to make all the elements of the array equal.
+// It is guaranteed that all the elements of the array can be made equal using some operations.
+//
+//Given an integer n, the length of the array, return the minimum number of operations needed to make all
+// the elements of arr equal.
