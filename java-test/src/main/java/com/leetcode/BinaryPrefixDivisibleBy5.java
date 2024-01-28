f0cb73ec4039976a5f94eab2f113aaf7ff0e7b0d
dkid <dimason2008usa@gmail.com>
2024-01-28 00:08:17 -0500

    from mac 01.27.2024

---
+++ java-test/src/main/java/com/leetcode/BinaryPrefixDivisibleBy5.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryPrefixDivisibleBy5 {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> result = new ArrayList<>();
+        int sum = 0;
+        for (int num : nums) {
+            sum = (sum * 2 + num) % 5;
+            if (sum != 0) {
+                result.add(false);
+            } else {
+                result.add(true);
+            }
+        }
+        return result;
+    }
+}
