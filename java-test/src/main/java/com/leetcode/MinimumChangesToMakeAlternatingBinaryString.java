af9617142ce94adfaf0507a126cf2f163d89d3d8
dkid <dimason2008usa@gmail.com>
2024-02-27 00:27:46 -0500

    from mac 02.26.2024

---
+++ java-test/src/main/java/com/leetcode/MinimumChangesToMakeAlternatingBinaryString.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+public class MinimumChangesToMakeAlternatingBinaryString {
+    public int minOperations(String s) {
+        int n = s.length();
+        int first = 0;
+        int second = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '1') first++;
+                else second++;
+            } else {
+                if (s.charAt(i) == '1') second++;
+                else first++;
+            }
+        }
+        return Math.min(first, second);
+    }
+}
