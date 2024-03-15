e3090f0d1048bd1e49ac6e3df2a1505c6745ce66
Dkid555 <dimason2008@gmail.com>
2024-03-14 21:24:31 -0400

    meta 03.14.2024

--- java-test/src/main/java/com/meta/other/graphs/Graph.java
+++ java-test/src/main/java/com/meta/other/graphs/Graph.java
@@ -1,21 +1,28 @@
 package com.meta.other.graphs;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Graph<T> {
-    private Map<T, List<T>> adjacencyList;
+    private Map<T, Set<T>> adjacencyList;
 
     public Graph() {
         adjacencyList = new HashMap<>();
     }
 
     public void addVertex(T vertex) {
-        adjacencyList.put(vertex, new ArrayList<>());
+        adjacencyList.put(vertex, new HashSet<>());
     }
 
+    public Set<T> getAllVertexes() {
+        return adjacencyList.keySet();
+    }
+
+    public boolean containsVertex(T vertex) {
+        return adjacencyList.containsKey(vertex);
+    }
     public void addEdge(T source, T destination) {
         if (!adjacencyList.containsKey(source)) {
             addVertex(source);
@@ -26,7 +33,7 @@ public class Graph<T> {
         adjacencyList.get(source).add(destination);
     }
 
-    public List<T> getNeighbors(T vertex) {
+    public Set<T> getNeighbors(T vertex) {
         return adjacencyList.get(vertex);
     }
 }
