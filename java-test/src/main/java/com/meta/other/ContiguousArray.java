6349da286c915037c3b46084057424f52fa11f70
dkid <dimason2008usa@gmail.com>
2024-04-02 21:23:51 -0400

    from mac 04.02.2024

---
+++ java-test/src/main/java/com/meta/other/ContiguousArray.java
@@ -0,0 +1,26 @@
+package com.meta.other;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContiguousArray {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> hashmap = new HashMap<>();
+        int zeros = 0, ones = 0, maxLen = 0;
+
+        hashmap.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) zeros++; else ones++;
+            int diff = zeros - ones;
+
+            if (hashmap.containsKey(diff)) {
+                maxLen = Math.max(maxLen, i - hashmap.get(diff));
+            } else {
+                hashmap.put(diff, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
